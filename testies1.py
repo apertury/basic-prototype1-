cfc311fd45697475e8b0a3a82d71ddbfeb032780
apertury <sathiyanarayanan_sai_emmanuel@students.edu.sg>
2024-10-15 11:08:33 +0800

    Update testies1.py

--- testies1.py
+++ testies1.py
@@ -1,7 +1,18 @@
 import time
 import datetime
 import pytz
+import getpass
 
 while True:
-  print("HI!!")
-  
+  time.sleep(0.5)
+  usr1 = input("Input New Username: ")
+  time.sleep(1)
+  pswrd1 = input("Input New Password: ")
+  time.sleep(0.5)
+  print(""), print("")
+  time.sleep(1)
+  entry = input("Input Username: ")
+  if entry in [usr1]:
+    entry2 = input("Input Password: ")
+    if entry2 in [pswrd1]:
+      print("Welcome!" + usr1)
