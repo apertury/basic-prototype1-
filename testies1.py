fa27b14a8fac5b0b5ff82e1913f273d0c7371c7b
apertury <sathiyanarayanan_sai_emmanuel@students.edu.sg>
2024-10-15 14:10:46 +0800

    Update testies1.py

--- testies1.py
+++ testies1.py
@@ -1,12 +1,34 @@
 import time
-import datetime
-import pytz
-import getpass
+import threading
+from datetime import datetime
+
+def logs():
+    log_counter = 1
+    logs = []
+
+    while True:
+        log_entry = input(f"Enter your log (LOG {log_counter}): ")
+        formatted_log = f"LOG {log_counter}: {log_entry}"
+        logs.append(formatted_log)
+        log_counter += 1
+
+        continue_logging = input("Add another? (yes/no): ").strip().lower()
+        if continue_logging != 'yes':
+            break
+        
+    tab = input("Show All Logs? (Y/N): ").strip().lower()
+    if tab in ['yes', 'y', 'Y']:
+        print("\nALL LOGS")
+        for log in logs:
+            print(log)
+
+current_datetime = datetime.now()
+formatted_datetime = current_datetime.strftime("%Y-%m-%d / %H:%M:%S")
 
 while True:
   time.sleep(0.5)
   usr1 = input("Input New Username: ")
-  time.sleep(1)
+  time.sleep(0.2)
   pswrd1 = input("Input New Password: ")
   time.sleep(0.5)
   print(""), print("")
@@ -15,4 +37,18 @@ while True:
   if entry in [usr1]:
     entry2 = input("Input Password: ")
     if entry2 in [pswrd1]:
-      print("Welcome!" + usr1)
+      print("Welcome! " + usr1.capitalize())
+      print("Entry Date/Time:", formatted_datetime)
+      print("\n")
+      time.sleep(1)
+      print("Options:"), time.sleep(1)
+      print("1. Log Life, 2. Exit")
+      print("3. U/C, 4. U/C")
+      kee = input(">: ")
+      if kee.lower() in ["1", "one"]:
+        logs(), print("\n"), time.sleep(1)
+        print("\n")
+      
+    else:
+      print("Incorrect Password / Username")
+      print("\n"), print("\n"), time.sleep(.5)
